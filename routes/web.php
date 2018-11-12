bffd6e741fce2f8beeee62f4bbdf880dfa8a6785
Anderson de Souza <anderson17ads@hotmail.com>
2018-11-12 11:21:12 -0200

    Configurando as rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index')->name('index');
+
+Route::get('/produto/{id}', 'HomeController@produto')->name('produto');
+
+Route::get('/carrinho', 'CarrinhoController@index')->name('carrinho.index');
+
+Route::get('/carrinho/adicionar', function() {
+    return redirect()->route('index');
 });
+
+Route::post('/carrinho/adicionar', 'CarrinhoController@adicionar')->name('carrinho.adicionar');
+
+Route::delete('/carrinho/remover', 'CarrinhoController@remover')->name('carrinho.remover');
+
+Route::post('/carrinho/concluir', 'CarrinhoController@concluir')->name('carrinho.concluir');
+
+Route::get('/carrinho/compras', 'CarrinhoController@compras')->name('carrinho.compras');
+
+Route::post('/carrinho/cancelar', 'CarrinhoController@cancelar')->name('carrinho.cancelar');
