0de42b3df8d9ef48bc148c4f65efe1310b526356
Anderson de Souza <anderson17ads@hotmail.com>
2018-11-13 09:48:33 -0200

    Ajustando estilo do detalhe produto

--- resources/views/produtos/view.blade.php
+++ resources/views/produtos/view.blade.php
@@ -5,20 +5,18 @@
 
 <div class="container">
     <div class="row">
-        <h3>{{ $produto->nome }}</h3>
-        <div class="divider"></div>
-        <div class="section col s12 m6 l4">
-            <div class="card small">
-                <img 
-                    class="col s12 m12 l12 materialboxed" 
-                    data-caption="{{ $produto->nome }}" 
-                    src="{{URL::asset('/image/uploads/produtos/')}}/{{ $produto->imagem }}" 
-                    alt="{{ $produto->nome }}" 
-                    title="{{ $produto->nome }}">
-            </div>
-        </div>
         <div class="section col s12 m6 l6">
-            <h4 class="left col l6"> R$ {{ number_format($produto->preco, 2, ',', '.') }} </h4>
+            <img 
+                class="col s12 m12 l12 materialboxed" 
+                data-caption="{{ $produto->nome }}" 
+                src="{{URL::asset('/image/uploads/produtos/')}}/{{ $produto->imagem }}" 
+                alt="{{ $produto->nome }}" 
+                title="{{ $produto->nome }}">
+        </div>
+        
+        <div class="produto-detalhe-detalhe section col s12 m6 l6">
+            <h3>{{ $produto->nome }}</h3>
+            <h4> R$ {{ number_format($produto->preco, 2, ',', '.') }} </h4>
             
             <form method="POST" action="{{ route('carrinho.adicionar') }}">
                 {{ csrf_field() }}
@@ -26,8 +24,10 @@
                 <button class="btn-large col l6 m6 s6 green accent-4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="O produto será adicionado ao seu carrinho">Comprar</button>   
             </form>
         </div>
-        <div class="section col s12 m6 l6">
-            {!! $produto->descricao !!}
+
+        <div class="produto-detalhe-descricao section col s12 m6 l6">
+            <h2>Descrição</h2>
+            <p>{!! $produto->descricao !!}</p>
         </div>
     </div>
 </div>
