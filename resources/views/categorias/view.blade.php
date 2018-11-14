2c5b05a3e093048844262a8e7fdbb65e4b739e36
Anderson de Souza <anderson17ads@hotmail.com>
2018-11-14 17:21:38 -0200

    Separando o bloco de listagem dos produtos

--- resources/views/categorias/view.blade.php
+++ resources/views/categorias/view.blade.php
@@ -11,26 +11,7 @@
 		@if ($categoria->categoriaProdutos)
 			<h3>Produtos</h3>
 
-			@foreach($categoria->categoriaProdutos as $produto)
-				<div class="col s12 m6 l4">
-					<div class="card medium">
-						<div class="card-image">
-							<img src="{{URL::asset('/image/uploads/produtos/')}}/{{ $produto->produto->imagem }}">
-						</div>
-						
-						<div class="card-content">
-							<span class="card-title grey-text text-darken-4 truncate" title="{{ $produto->produto->nome }}">
-								{{ $produto->produto->nome }}
-							</span>
-							<p>R$ {{ number_format($produto->produto->preco, 2, ',', '.') }}</p>
-						</div>
-
-						<div class="card-action">
-							<a class="blue-text" href="{{ route('produto', $produto->produto->id) }}">Veja mais</a>
-						</div>
-					</div>
-				</div>
-			@endforeach
+			@include('produtos._produtos', ['produtos' => $categoria->categoriaProdutos])
 		@endif
 	</div>
 </div>
