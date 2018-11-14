a7d5b71a8a59de7da4f51bb0d7f0fbbba644d3f3
Anderson de Souza <anderson17ads@hotmail.com>
2018-11-14 17:11:50 -0200

    Ajustando form do login e cadastro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,32 +6,33 @@
 <div class="container">
     <div class="row">
         <div class="col l6 offset-l3 s12 m10 offset-m2">
-            
-            <h4>Login</h4>
-            <form method="POST" action="{{ url('/login') }}">
-                {{ csrf_field() }}
+            <h3>Entrar</h3>
 
-                @include('auth._form_email')
-                @include('auth._form_password')
+            <div class="col l12 s12 m12">
+                <form method="POST" action="{{ url('/login') }}">
+                    {{ csrf_field() }}
 
-                <div class="row">
-                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}} id="remember" />
-                    <label for="remember">Lembre-se de mim</label>
-                </div>
+                    @include('auth._form_email')
+                    @include('auth._form_password')
 
-                <div class="row">
-                    <button type="submit" class="btn blue col l8 s12 m8">
-                        Entrar
-                    </button>
-                </div>
+                    <div class="row">
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}} id="remember" />
+                        <label for="remember">Lembre-se de mim</label>
+                    </div>
 
-                <div class="row">
-                    <a class="" href="{{ url('/password/reset') }}">
-                        Esqueceu sua senha?
-                    </a>
-                </div>
+                    <div class="row">
+                        <button type="submit" class="btn col l8 s12 m8">
+                            Entrar
+                        </button>
+                    </div>
 
-            </form>
+                    <div class="row">
+                        <a class="" href="{{ url('/password/reset') }}">
+                            Esqueceu sua senha?
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
 
     </div>
